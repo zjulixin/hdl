99a1a94148c78e2f45f0aa037e7d718db45a1fc4
Sergiu Arpadi <sergiu.arpadi@analog.com>
2020-03-15 13:26:41 +0000

    sysid_intel: Added sysid to intel projects

--- projects/adrv9371x/a10gx/system_qsys.tcl
+++ projects/adrv9371x/a10gx/system_qsys.tcl
@@ -1,6 +1,7 @@
 
 set dac_fifo_address_width 10
 
+source $ad_hdl_dir/projects/scripts/adi_sysid_intel.tcl
 source $ad_hdl_dir/projects/common/a10gx/a10gx_system_qsys.tcl
 source $ad_hdl_dir/projects/common/intel/dacfifo_qsys.tcl
 source ../common/adrv9371x_qsys.tcl
@@ -56,4 +57,13 @@ set_interconnect_requirement {mm_interconnect_0|router_026.src/rsp_demux_016.sin
 set_interconnect_requirement {mm_interconnect_0|router_027.src/rsp_demux_017.sink} {qsys_mm.postTransform.pipelineCount} {1}
 set_interconnect_requirement {mm_interconnect_0|router_022.src/rsp_demux_012.sink} {qsys_mm.postTransform.pipelineCount} {1}
 set_interconnect_requirement {mm_interconnect_0|router_023.src/rsp_demux_013.sink} {qsys_mm.postTransform.pipelineCount} {1}
+
+#system ID
+set_instance_parameter_value axi_sysid_0 {ROM_ADDR_BITS} {9}
+set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
+
+set_instance_parameter_value rom_sys_0 {PATH_TO_FILE} "[pwd]/mem_init_sys.txt"
+
+set sys_cstring "sys rom custom string placeholder";
+sysid_gen_sys_init_file $sys_cstring;
 
--- projects/daq2/a10gx/system_qsys.tcl
+++ projects/daq2/a10gx/system_qsys.tcl
@@ -1,6 +1,7 @@
 
 set dac_fifo_address_width 10
 
+source $ad_hdl_dir/projects/scripts/adi_sysid_intel.tcl
 source $ad_hdl_dir/projects/common/a10gx/a10gx_system_qsys.tcl
 source $ad_hdl_dir/projects/common/intel/dacfifo_qsys.tcl
 source ../common/daq2_qsys.tcl
@@ -20,3 +21,13 @@ set_interconnect_requirement {mm_interconnect_0|sys_ddr3_cntrl_ctrl_amm_0_to_axi
 set_interconnect_requirement {mm_interconnect_0|sys_ddr3_cntrl_ctrl_amm_0_to_axi_ad9144_dma_m_src_axi_wr_rsp_width_adapter.src/async_fifo_008.in} {qsys_mm.postTransform.pipelineCount} {1}
 set_interconnect_requirement {mm_interconnect_0|sys_ddr3_cntrl_ctrl_amm_0_to_axi_ad9144_dma_m_src_axi_rd_rsp_width_adapter.src/async_fifo_009.in} {qsys_mm.postTransform.pipelineCount} {1}
 set_interconnect_requirement {mm_interconnect_0|sys_ddr3_cntrl_ctrl_amm_0_to_sys_ethernet_dma_tx_mm_read_rsp_width_adapter.src/async_fifo_010.in} {qsys_mm.postTransform.pipelineCount} {1}
+
+#system ID
+set_instance_parameter_value axi_sysid_0 {ROM_ADDR_BITS} {9}
+set_instance_parameter_value rom_sys_0 {ROM_ADDR_BITS} {9}
+
+set_instance_parameter_value rom_sys_0 {PATH_TO_FILE} "[pwd]/mem_init_sys.txt"
+
+set sys_cstring "sys rom custom string placeholder";
+sysid_gen_sys_init_file $sys_cstring;
+
